5cb01298b2167dd63e56d6bddfede41aed13ed8e
GVladislav <grishinvladislav97@gmail.com>
2023-06-06 23:00:21 +0300

    Fix session commits (db)

--- src/alchemy/db_handler.py
+++ src/alchemy/db_handler.py
@@ -5,6 +5,7 @@ from config import *
 from datetime import datetime
 from sqlalchemy_utils import database_exists, create_database
 
+
 class DbHandler:
     def __init__(self, DSN):
         self.__engine = sqlalchemy.create_engine(DSN)
@@ -31,7 +32,7 @@ class DbHandler:
                             max_tokens=REPLY_MAX_TOKENS,
                             model=REPLY_MODEL)
         self.__s.add(conv)
-        self.__s.commit()
+        self.commit()
         return conv.id
 
     def record_character(self, conversation_id, name, prompt):
@@ -44,7 +45,7 @@ class DbHandler:
                          model=GEN_MODEL,
                          prompt=prompt)
         self.__s.add(char)
-        self.__s.commit()
+        self.commit()
         return char.id
 
     def record_message(self, character_id, conversation_id, text,
@@ -53,12 +54,18 @@ class DbHandler:
                       conversation_id=conversation_id,
                       user=user, text=text)
         self.__s.add(mes)
-        self.__s.commit()
+        self.commit()
 
     def mark_conversation_win(self, state: bool, conversation_id):
         conv = self.__s.query(Conversation).filter(
             Conversation.id == conversation_id).update({'win': state})
-        self.__s.commit()
+        self.commit()
+
+    def commit(self):
+        try:
+            self.__s.commit()
+        except:
+            self.__s.rollback()
 
     @staticmethod
     def __create_tables(engine) -> None:

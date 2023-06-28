c99b26e449b24101f5ca0c2c9a6fa1b1b4449f18
GVladislav <grishinvladislav97@gmail.com>
2023-06-28 15:44:32 +0300

    Add daily quota reset func

---
+++ src/schedule/reset_quota.py
@@ -0,0 +1,28 @@
+import asyncio
+from datetime import datetime
+
+from aiogram import dispatcher
+
+from config import QUOTED_CHATS
+
+
+async def daily_quota_reset(dp: dispatcher, db):
+    while True:
+
+        current_time = datetime.now()
+        target_time = datetime(year=current_time.year,
+                               month=current_time.month,
+                               day=current_time.day,
+                               hour=15,
+                               minute=00)
+
+        delta = target_time - current_time
+
+        await asyncio.sleep(delta.seconds + 1)
+
+        db.reset_quota_for_all()
+
+        for chat, quota in QUOTED_CHATS.items():
+            await dp.bot.send_message(chat_id=chat,
+                                      text=f'Квота восстановлена. '
+                                           f'({quota})')
